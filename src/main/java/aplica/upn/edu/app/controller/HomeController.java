0553b6b783f1d9fa1c7f1a341654301860c2b67b
Anthony Castañeda Ordaya <raco90_@hotmail.com>
2020-09-04 04:06:05 -0500

    planilla de empleados listo

---
+++ src/main/java/aplica/upn/edu/app/controller/HomeController.java
@@ -0,0 +1,71 @@
+package aplica.upn.edu.app.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import aplica.upn.edu.app.model.Empleado;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public ModelAndView irHome() {
+
+		ModelAndView MV= new ModelAndView();
+
+		MV.setViewName("home");
+		return MV;         //Retorna el nombre del archivo jsp de la vista 
+		//Por lo tanto debemos crear un archivo llamado home.jsp
+	}
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		List<Empleado> empleados=getLista();
+		//peliculas.add("A");
+		//peliculas.add("B");
+		//peliculas.add("C");
+
+		model.addAttribute("Vempleado",empleados);
+		return "home";
+	}
+
+	private List<Empleado>getLista(){
+		SimpleDateFormat formatter =new SimpleDateFormat("dd-MM-yyyy");
+		List<Empleado> lista =null;
+		try {
+			lista = new LinkedList<>();
+			Empleado e = new Empleado();
+			e.setId(1); e.setNombre("Anthony"); e.setApellido("Castañeda");
+			e.setCargo("programador"); e.setFechaInicio(formatter.parse("02-05-2018"));
+			e.setFechaFin(formatter.parse("02-05-2018"));
+			e.setEstado("Activo");e.setTelefono(123); 
+			e.setImagen("empleado.png");
+			lista.add(e);
+			return lista;
+		}
+		catch(Exception e){
+			return null;
+		}
+
+	}
+
+
+	/* @RequestMapping(value="/detalle")
+    public String mostrarDetalle(Model model) {
+    	String tituloPelicula = "Anabelle";
+    	int duracion=136;
+    	double precio =50;
+    	model.addAttribute("titulo",tituloPelicula);
+    	model.addAttribute("duracion",duracion);
+    	model.addAttribute("precio",precio);
+    	return "detalle";
+
+    }*/
+
+}
